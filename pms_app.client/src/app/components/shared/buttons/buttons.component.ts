e588b3353fa74a7cafd7792d4e2f0470c457b277
Phuti-SoftElsie <phuti.motimele@softelsie.com>
2025-10-01 11:41:35 +0200

    Buttons styles moved to global and inputs reduced

--- pms_app.client/src/app/components/shared/buttons/buttons.component.ts
+++ pms_app.client/src/app/components/shared/buttons/buttons.component.ts
@@ -2,11 +2,11 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 
 @Component({
-  selector: 'buttons',
+  selector: 'app-button',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './buttons.component.html',
-  styleUrl: './buttons.component.css'
+ 
 
 })
 export class ButtonsComponent {
@@ -14,8 +14,5 @@ export class ButtonsComponent {
   @Input() variant: 'primary' | 'secondary' = 'primary';
   @Input() size: 'xl' | 'lg' | 'md' | 'sm' | 'xs' = 'md';
   @Input() disabled: boolean = false;
-  @Input() selected: boolean = false;
-  @Input() darkMode: boolean = false;
-  @Input() isHovered: boolean = false;
 }
 
